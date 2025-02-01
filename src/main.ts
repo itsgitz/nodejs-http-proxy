be5eb10b75c71f9cc29b5215caa39405f3baab4b
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-02-01 21:20:01 +0700

    chore: updated auth service port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const servicesUrl: Record<string, string> = {
-  AUTH: "http://localhost:3000/api/v1/auth",
+  AUTH: "http://localhost:8080/api/v1/auth",
   UTILS: "http://localhost:3001/api/v1/utils",
   COMMERCIAL: "http://localhost:3002/api/v1/commercial",
   PNO: "http://localhost:3003/api/v1/pno",
